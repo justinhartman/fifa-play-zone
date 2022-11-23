2580ee6c26fff960ec9feee00cfca4fddfbd7418
Justin Hartman <code@justhart.com>
2022-11-23 22:43:44 +0200

    refactor: App component to render new screen tabs navigator

--- App.jsx
+++ App.jsx
@@ -1,20 +1,73 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import BracketChallenge from './screens/BracketChallenge';
+import DailyFantasy from './screens/DailyFantasy';
+import FantasyLeague from './screens/FantasyLeague';
+import MatchPredictor from './screens/MatchPredictor';
 
+const Tab = createBottomTabNavigator();
+
+/**
+ * A function that returns a React component that renders the bottom tab navigator.
+ *
+ * @function App
+ * @returns {JSX.Element}
+ * @constructor
+ */
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator
+        initialRouteName="Fantasy League"
+        screenOptions={{
+          tabBarActiveTintColor: '#e91e63',
+        }}>
+        <Tab.Screen
+          name="Fantasy League"
+          component={FantasyLeague}
+          options={{
+            headerShown: false,
+            tabBarLabel: 'Fantasy',
+            tabBarIcon: ({ color, size }) => (
+              <MaterialCommunityIcons name="soccer" color={color} size={size} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Daily Fantasy"
+          component={DailyFantasy}
+          options={{
+            headerShown: false,
+            tabBarLabel: 'Daily',
+            tabBarIcon: ({ color, size }) => (
+              <MaterialCommunityIcons name="calendar-today" color={color} size={size} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Match Predictor"
+          component={MatchPredictor}
+          options={{
+            headerShown: false,
+            tabBarLabel: 'Predictor',
+            tabBarIcon: ({ color, size }) => (
+              <MaterialCommunityIcons name="counter" color={color} size={size} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Bracket Challenge"
+          component={BracketChallenge}
+          options={{
+            headerShown: false,
+            tabBarLabel: 'Bracket',
+            tabBarIcon: ({ color, size }) => (
+              <MaterialCommunityIcons name="sitemap" color={color} size={size} />
+            ),
+          }}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
