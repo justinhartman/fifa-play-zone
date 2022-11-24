bc6d7376a756b038c0cfc9fa622064e61137ab9f
Justin Hartman <code@justhart.com>
2022-11-24 12:49:20 +0200

    refactor: Change babel.config.js to use anonymous function

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,10 @@
-module.exports = function(api) {
+/**
+ * Babel configuration.
+ *
+ * @param api
+ * @returns {{presets: string[]}}
+ */
+module.exports = api => {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
