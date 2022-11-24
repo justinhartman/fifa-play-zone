3b24aeab1de67528916eb01f95c7e2672a289e5c
Justin Hartman <code@justhart.com>
2022-11-24 12:56:38 +0200

    feat: Add Daily Challenge screen and tab navigation

--- App.jsx
+++ App.jsx
@@ -8,6 +8,7 @@ import { MaterialCommunityIcons } from '@expo/vector-icons';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import BracketChallenge from './screens/BracketChallenge';
+import DailyChallenge from './screens/DailyChallenge';
 import DailyFantasy from './screens/DailyFantasy';
 import FantasyLeague from './screens/FantasyLeague';
 import MatchPredictor from './screens/MatchPredictor';
@@ -25,10 +26,21 @@ export default function App() {
   return (
     <NavigationContainer>
       <Tab.Navigator
-        initialRouteName="Fantasy League"
+        initialRouteName="Daily Challenge"
         screenOptions={{
           tabBarActiveTintColor: '#e91e63',
         }}>
+        <Tab.Screen
+          name="Daily Challenge"
+          component={DailyChallenge}
+          options={{
+            headerShown: false,
+            tabBarLabel: 'Challenge',
+            tabBarIcon: ({ color, size }) => (
+              <MaterialCommunityIcons name="check-decagram" color={color} size={size} />
+            ),
+          }}
+        />
         <Tab.Screen
           name="Fantasy League"
           component={FantasyLeague}
---
+++ screens/DailyChallenge.jsx
@@ -0,0 +1,46 @@
+/**
+ * Daily Challenge Webview Screen Component.
+ *
+ * @author    Justin Hartman <code@justhart.com>
+ * @copyright Copyright (c) 2022 Justin Hartman <https://justhart.com>
+ */
+import { StyleSheet, SafeAreaView, StatusBar } from 'react-native';
+import { WebView } from 'react-native-webview';
+import LoadingScreen from '../components/LoadingScreen.jsx';
+
+/**
+ * A function that returns a React component that renders the Daily Challenge webview.
+ *
+ * @returns {JSX.Element}
+ * @constructor
+ */
+function DailyChallenge() {
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <StatusBar barStyle="dark-content" />
+      <WebView
+        style={styles.container}
+        bounces={false}
+        originWhitelist={['*']}
+        source={{ uri: 'https://play.fifa.com/daily-challenge/' }}
+        renderLoading={() => <LoadingScreen />}
+        startInLoadingState={true}
+        onError={() => {
+          alert('Error loading page');
+        }}
+      />
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'transparent',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: -40,
+  },
+});
+
+export default DailyChallenge;
