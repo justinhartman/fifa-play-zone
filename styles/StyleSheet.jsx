2db4bd78f32a99fc3fab65b891b49d6825389b43
Justin Hartman <code@justhart.com>
2022-11-26 15:23:20 +0200

    style: Apply style linting to StyleSheet.jsx

--- styles/StyleSheet.jsx
+++ styles/StyleSheet.jsx
@@ -16,7 +16,7 @@ import { StyleSheet } from 'react-native';
 const GlobalStyles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center"
+    justifyContent: 'center',
   },
 });
 
